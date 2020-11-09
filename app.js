87700eed6674379325065eb20ad852cc8d15fb1b
manuelsanchez2 <manusansan22@gmail.com>
2020-11-09 08:56:38 +0100

    Create a separate js file

---
+++ app.js
@@ -0,0 +1,8 @@
+function imgSlider(anything) {
+  document.querySelector(".pasta").src = anything;
+}
+
+function changeCircleColor(color) {
+  const circle = document.querySelector(".circle");
+  circle.style.background = color;
+}
