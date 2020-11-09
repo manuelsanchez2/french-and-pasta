f575fa07ab4493ec2081e2ddffef42add873abcb
manuelsanchez2 <manusansan22@gmail.com>
2020-11-09 09:52:50 +0100

    Work with responsiveness

--- app.js
+++ app.js
@@ -6,3 +6,9 @@ function changeCircleColor(color) {
   const circle = document.querySelector(".circle");
   circle.style.background = color;
 }
+function menuToggle() {
+  const toggleMenu = document.querySelector(".toggleMenu");
+  const navigation = document.querySelector(".navigation");
+  toggleMenu.classList.toggle("active");
+  navigation.classList.toggle("active");
+}
